7af2d7f9ebdc3ac2bd01ab850e386f16ee14aa67
prafful <prraful@gmail.com>
2020-11-25 09:45:57 +0530

    state

--- react_example/myfirstreact/src/review/reviewaction.js
+++ react_example/myfirstreact/src/review/reviewaction.js
@@ -3,15 +3,40 @@ import React from 'react';
 
 class ReviewAction extends React.Component {
 
-    like = this.props.mylike
-    dislike = this.props.mydislike
+    constructor(props){
+       super(props) 
+       this.state = {
+           like: parseInt(this.props.mylike),
+           dislike: parseInt(this.props.mydislike)
+       }
+
+    }
+
+    //like = this.props.mylike
+    //dislike = this.props.mydislike
+
+    addLikes = ()=>{
+        console.log(this.state.like)
+        //uncorrect way!
+        //this.state.like++
+        this.setState({like: this.state.like+1})
+        console.log(this.state.like)
+    }
+
+    removeLikes = ()=>{
+        console.log(this.state.dislike)
+        //uncorrect way!
+        //this.state.dislike++
+        this.setState({dislike: this.state.dislike+1})
+        console.log(this.state.dislike)
+    }
    
     render() { 
         return ( 
             <div>
-                <button>Like {this.like} </button>
+                <button onClick={this.addLikes}>Like {this.state.like} </button>
                 &nbsp;
-                <button>Dislike {this.dislike} </button>
+                <button onClick={this.removeLikes}>Dislike {this.state.dislike} </button>
             </div> 
         )
     }
