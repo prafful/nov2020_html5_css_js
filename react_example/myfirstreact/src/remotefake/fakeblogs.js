f58f785785326848ece1afe7e1cb530efeb8b4de
prafful <prraful@gmail.com>
2020-11-30 09:50:45 +0530

    add blog done

--- react_example/myfirstreact/src/remotefake/fakeblogs.js
+++ react_example/myfirstreact/src/remotefake/fakeblogs.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import axios from "axios";
-import SingleBlog from './fakesingleblog';
+import axios from "axios"
+import SingleBlog from './fakesingleblog'
 
 
 class FakeBlogs extends React.Component {
@@ -8,7 +8,11 @@ class FakeBlogs extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            posts: []
+            posts: [],
+            displayAddBlogStatus: false,
+            newBlogTitle:"",
+            newBlogDescription:"",
+            newBlogUserId: 0
         }
     }
     
@@ -44,20 +48,91 @@ class FakeBlogs extends React.Component {
         )
     }
 
-    addBlog =()=>{
+    addBlog =(event)=>{
+
+        event.preventDefault()
+
         var blogItem = {
-                "userId": 4,
-                "title": "triple screen mobiles",
-                "body": "LG 9x thinq and lg velvet v90 come with folding triple screen support!",
+                "userId": this.state.newBlogUserId,
+                "title": this.state.newBlogTitle,
+                "body": this.state.newBlogDescription,
             }
         axios.post("http://localhost:1234/blogs", blogItem)
                 .then(response=>{
                     console.log(response)
                     this.getBlogs()
+                    this.toggleDisplayAddBlog()
+                    this.setState({
+                        newBlogTitle:"",
+                        newBlogDescription:"",
+                        newBlogUserId: 0
+                    })
                 })
                 .catch(error=>{
-                    console.log(error);
+                    console.log(error)
                 })
+    }
+
+    toggleDisplayAddBlog = ()=>{
+        this.setState({
+            displayAddBlogStatus: !this.state.displayAddBlogStatus
+        })
+    }
+
+    captureTitle =(event)=>{
+        console.log(event)
+        console.log(event.target)
+        console.log(event.target.value)
+        this.setState({
+            newBlogTitle: event.target.value
+        })
+    }
+
+    captureDescription=(event)=>{
+        console.log(event.target.value)
+        this.setState({
+            newBlogDescription: event.target.value
+        })
+    }
+
+    captureUserId =(event)=>{
+        console.log(event.target.value)
+        this.setState({
+            newBlogUserId: event.target.value
+        })
+    }
+
+    cancelAddBlog=()=>{
+        this.toggleDisplayAddBlog()
+        this.setState({
+            newBlogTitle:"",
+            newBlogDescription:"",
+            newBlogUserId: 0
+        })
+    }
+
+    displayAddBlog =()=>{
+        if(this.state.displayAddBlogStatus){
+            return (
+                <div>
+                  <form>
+                      User Id: 
+                      <input type="text" onChange={this.captureUserId}></input>
+                      <br></br>
+                      Title: 
+                      <input type="text" onChange={this.captureTitle}></input>
+                      <br></br>
+                      Description: 
+                      <textarea onChange={this.captureDescription}></textarea>
+                      <br></br>
+                      <button onClick={this.addBlog}>Add</button>
+                      &nbsp;
+                      <button onClick={this.cancelAddBlog}>Cancel</button>
+                  </form>
+                </div>
+            )
+        }
+    
     }
 
     render() { 
@@ -65,8 +140,10 @@ class FakeBlogs extends React.Component {
             <div>
                 <hr></hr>
                 <h1>List of blogs...</h1>
-                <button onClick={this.addBlog}>Add Blog....</button>
+                <button onClick={this.toggleDisplayAddBlog}>Add Blog....</button>
                 <br></br>
+                <br></br>
+                {this.displayAddBlog()}
                 <br></br>
                 <table border="1">
                     <thead>
